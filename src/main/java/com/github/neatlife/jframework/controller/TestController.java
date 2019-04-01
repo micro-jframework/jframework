31deeedc467a42fe908eaa2f383652c423700a00
suxiaolin <dear.lin@live.com>
2019-04-01 21:52:08 +0800

    add image display and download code example

--- src/main/java/com/github/neatlife/jframework/controller/TestController.java
+++ src/main/java/com/github/neatlife/jframework/controller/TestController.java
@@ -1,9 +1,20 @@
 package com.github.neatlife.jframework.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
  * @author suxiaolin
@@ -20,4 +31,26 @@ public class TestController extends Controller {
         return "hello world.";
     }
 
+    @GetMapping(value = "/image", produces = MediaType.IMAGE_PNG_VALUE)
+    public void getImage(HttpServletResponse response) throws IOException {
+
+        FileInputStream fileInputStream = new FileInputStream(new File("src/main/resources/1.png"));
+
+        response.setContentType(MediaType.IMAGE_PNG_VALUE);
+        StreamUtils.copy(fileInputStream, response.getOutputStream());
+    }
+
+    @GetMapping(path = "/download")
+    public ResponseEntity<Resource> download(String param) throws IOException {
+        File file = new File("src/main/resources/1.png");
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+
+        HttpHeaders headers = new HttpHeaders(); headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=1.png");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(resource);
+    }
 }
