8c85b6051d196c2514f50da8343779eb0035cefe
suxiaolin <dear.lin@live.com>
2019-05-14 14:56:41 +0800

    add apollo maven dependency

--- src/main/java/com/github/neatlife/jframework/JframeworkApplication.java
+++ src/main/java/com/github/neatlife/jframework/JframeworkApplication.java
@@ -1,5 +1,6 @@
 package com.github.neatlife.jframework;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author suxiaolin
  */
 @SpringBootApplication
+@EnableApolloConfig
 public class JframeworkApplication {
 
     public static void main(String[] args) {
